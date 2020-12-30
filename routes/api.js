404b80f5acef9e09031a54c6230aa2bf1378910b
Raviteja-205 <avadhanula.sai.16cse@bml.edu.in>
2020-12-30 16:24:28 +0530

    first

---
+++ routes/api.js
@@ -0,0 +1,50 @@
+const express = require('express');
+//const Posts = require('../../../React-Tutorials-master/React-Tutorials-master/React and HTTP/react-http/models/posts');
+
+const router = express.Router();
+
+const Posts = require('../models/posts');
+
+
+// Routes
+router.get('/posts', (req, res) => {
+
+    Posts.find({  })
+        .then((data) => {
+            //console.log('Data: ', data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log('error: ', daerrorta);
+        });
+});
+
+router.post('/save', (req, res) => {
+    const data = req.body;
+
+    const newBlogPost = new Posts(data);
+
+    newBlogPost.save((error) => {
+        if (error) {
+            res.status(500).json({ msg: 'Sorry, internal server errors' });
+            return;
+        }
+        // BlogPost
+        return res.json({
+            msg: 'Your data has been saved!!!!!!'
+        });
+    });
+});
+
+
+router.get('/name', (req, res) => {
+    const data =  {
+        username: 'peterson',
+        age: 5
+    };
+    res.json(data);
+});
+
+
+
+module.exports = router;
